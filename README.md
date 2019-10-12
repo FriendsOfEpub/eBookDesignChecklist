876e40348a26749cd9e7fff43476d7b94795e241
JayPanoz <panozpublishingteam@gmail.com>
2019-10-12 12:33:12 +0200

    Fix scroll in latest Safari + disable motion if global pref set

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Thanks to service workers, it will even work offline on Android, Chrome, Opera o
 
 The eBook Design Checklist is part of the Blitz Project. It’s a progressive web app which aim is to help you design your eBooks.
 
-It is currently in v.1.0.1 and available [on this page](https://friendsofepub.github.io/eBookDesignChecklist/).
+It is currently in v.1.0.2 and available [on this page](https://friendsofepub.github.io/eBookDesignChecklist/).
 
 ## Details
 
--- js/script.js
+++ js/script.js
@@ -19,9 +19,10 @@ r(function() {
 			checked = 0,
 			progress = 0;
 			
-	var isFirefox = false,
-			isChrome = false,
-			isEdge = false;
+	var isFirefox = false;
+
+	var mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)'),
+			noMotion = false;
 	
 	// POLYFILLS
 	
@@ -130,22 +131,20 @@ r(function() {
 	};
   
 	function scrollTo(element, to, duration) {
-		if (duration <= 0) return;
-		var difference = to - element.scrollTop;
-		var perTick = difference / duration * 10;
-		setTimeout(function() {
-			element.scrollTop = element.scrollTop + perTick;
-			if (element.scrollTop == to) return;
-			scrollTo(element, to, duration - 10);
-		}, 10);
+		if (!noMotion) {
+			if (duration <= 0) return;
+			var difference = to - element.scrollTop;
+			var perTick = difference / duration * 10;
+			setTimeout(function() {
+				element.scrollTop = element.scrollTop + perTick;
+				if (element.scrollTop == to) return;
+				scrollTo(element, to, duration - 10);
+			}, 10);
+		}
 	};
 
 	function scrollTop() {
-	  if (isFirefox && !isEdge || isChrome && !isEdge) {
-    	scrollTo(document.getElementsByTagName('html')[0], form.offsetTop, 600);
-		} else {
-			scrollTo(document.body, form.offsetTop, 600);
-		}
+    scrollTo(document.scrollingElement, form.offsetTop, 600);
 	};
 	
 	function focusNoScroll(el) {
@@ -192,6 +191,14 @@ r(function() {
 		toggleAria(help);
 	};
 
+	function changeMotionHook() {
+		if (mediaQuery.matches) {
+			noMotion = true;
+		} else {
+			noMotion = false;
+		}
+	}
+
 	// INIT (run on doc Ready)
 	
 	(function checkFirefox() {
@@ -200,16 +207,8 @@ r(function() {
 		}
 	})();
 
-	(function checkChrome() {
-		if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-			isChrome = true;
-		}
-	})();
-
-	(function checkEdge() {
-		if (navigator.userAgent.toLowerCase().indexOf('edge') > -1) {
-			isEdge = true;
-		}
+	(function initMotionHook() {
+		changeMotionHook();
 	})();
 		
 	(function hideDetails() {
@@ -289,6 +288,8 @@ r(function() {
 	})();
 	
 	// Event Listeners 
+
+	mediaQuery.addListener(changeMotionHook);
 	
 	form.addEventListener('click', function(e) {
 		var elt = e.target;
