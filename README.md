6f23f5ecc66440eb00a00e280c2867a72febc373
JayPanoz <panozpublishingteam@gmail.com>
2020-01-09 10:37:30 +0100

    Add boilerplate and template

--- README.md
+++ README.md
@@ -1,2 +1,254 @@
 # blitz-checklist
+
 A template repository to make checklist web apps
+
+This tool is part of the [Blitz Framework](http://friendsofepub.github.io/Blitz/) and is the tiny little engine that powers the [BlitzOptim](https://friendsofepub.github.io/eBookDesignChecklist/) and [BlitzPerf](https://friendsofepub.github.io/eBookPerfChecklist/) web apps.
+
+## Abstract
+
+Blitz checklist is a template repository leveraging [Mustache](http://mustache.github.io) to create interactive checklists. It provides all assets required for a Progressive Web App and a JSON model to generate its index page.
+
+## Install
+
+First make sure you have nodeJS and npm installed. If you don’t [install it](https://nodejs.org/).
+
+Then either clone this repository:
+
+```
+git clone https://github.com/FriendsOfEpub/blitz-checklist.git
+```
+
+Or “use this template” on github, which will create your own repository with the same files and folders, and clone yours.
+
+Finally `cd` into the project and:
+
+```
+npm install
+```
+
+This will install Mustache as a dev dependency.
+
+## Build
+
+When you need to generate a new `index.html`, run: 
+
+```
+npm run build
+```
+
+This will use `data.json` to generate your web app.
+
+## Keep in Sync With the Template Repository
+
+You’ll have to add the template repository as a remote:
+
+```
+git remote add template https://github.com/FriendsOfEpub/blitz-checklist.git
+```
+
+Then fetch when you want to update the changes:
+
+```
+git fetch --all
+```
+
+Then to merge the changes in the template, try:
+
+```
+git merge template/[branch to merge] --allow-unrelated-histories
+```
+
+Yet expect to resolve some conflicts for the merge to go through, as the 2 repos have unrelated histories.
+
+## Customize Your Web App
+
+Blitz checklist provides everything required to build a Progressive Web App, but it should be considered **a boilerplate you have to customize.**
+
+### Customize Assets
+
+In `assets` you will find an `icons` folder where you should put the web app’s icons. This will be used on the springboard when installing the app on iOS for instance, or home screen on Android. The recommended sizes are `256 * 256` and `512 * 512` – so that Android can generate a splash screen –, in `PNG` format. Note you can change these in `manifest.json` if needed.
+
+The logo will be used in the header of the web app.
+
+Don’t forget to customize your favicon!
+
+### Customize Styles
+
+In `css` you will find a global stylesheet, and one specific to print – users can indeed print the checklist.
+
+Note the `name` you are using for sections in `data.json` is used extensively if you want to add section-specific styles:
+
+- the section’s `id` has a `-list` prefix e.g. if the `name` has the value of `css` then the `id` is `css-list`;
+- its heading’s `id` (`h2`) has a `-heading` prefix e.g. `css-heading`.
+
+In addition, the `name` attribute of the section’s `inputs` use this value.
+
+### Customize JS
+
+In `js` you will find a script.
+
+You must at least customize `STORAGE_PREFIX` at the top of the file.
+
+You may also want to customize `HELP_TEXT` if the default one doesn’t suit you.
+
+### Customize the Manifest
+
+In `manifest.json` you must at least customize:
+
+- `name`
+- `short_name`
+- `start_url`
+- `background_color`
+- `theme_color`
+
+[See MDN for further details](https://developer.mozilla.org/en-US/docs/Web/Manifest).
+
+### Customize the Service Worker
+
+In `sw.js` you must at least customize:
+
+- `APP_PREFIX`: the prefix that will be used to cache contents;
+- `SCOPE`: the scope of your Service Worker (the path it controls);
+- `VERSION`: the current version of the app (to update every time you push a new version).
+
+These variables are used for the name of the cache, and the URLs the Service Worker will cache/handle. Please be very cautious as this is very easy to mess up on an update.
+
+If you’re not familiar with Service Workers, [check this introduction](https://developers.google.com/web/fundamentals/primers/service-workers).
+
+### Values Must Match
+
+Some values in `manifest.json`, `data.json`, `sw.js` and `script.js` should match if you want the web app to be consistent or function properly (scope).
+
+| manifest.json    | data.json      | sw.js      | script.js      |
+|------------------|----------------|------------|----------------|
+| short_name       | appName        | N/A        | N/A            |
+| start_url        | appScope       | SCOPE      | N/A            |
+| background_color | accentColor    | N/A        | N/A            |
+| theme_color      | accentColor    | N/A        | N/A            |
+| N/A              | footer.version | VERSION    | N/A            |
+| N/A              | N/A            | APP_PREFIX | STORAGE_PREFIX |
+
+## Create a Checklist
+
+To create an `index.html` file, you must first populate `src/data.json` with contents. Indeed this is what `src/template.mustache` will use to generate the checklist app.
+
+### appName
+
+The `appName` property expects a string. This will be used in the `title` of the document and SHOULD match the manifest’s `short_name`.
+
+### appVersion
+
+The `appVersion` SHOULD match the value of the `VERSION` variable in both `src/script.js` and `sw.js`. It will be displayed in the footer of the app.
+
+### appDesc
+
+The `appDesc` property expects a string. This will be used in the `<meta name="description">` of the document to improve SEO.
+
+### appLogo
+
+The `appLogo` property expects the path to your logo.
+
+If the value is set to `null` or an empty string (`""`), no logo (`img`) will be rendered.
+
+### appScope
+
+The `appScope` property expects the path Service Workers should handle. **It MUST match the manifest’s `start_url` and Service Worker’s `SCOPE`.**
+
+### lang
+
+The `lang` property expects a [BCP-47 language tag](https://tools.ietf.org/html/bcp47) e.g. `en`, `en-US`, etc. It will be used to set the language of the document.
+
+### accentColor
+
+The `accentColor` property expects an HEX value e.g. `#000000`. It will be used to customize the web app’s theming, and SHOULD match the manifest’s `background_color` and `theme_color`.
+
+### header
+
+The `header` property expects an object:
+
+```
+{
+  "title": "h1 of the page"
+  "lead": [
+    "Desc of your web app"
+  ]
+}
+```
+
+The `title` will be used for the `h1` of the webpage.
+
+The `lead` property expects an array of strings. It serves as a description of the web app. For each value in the array, a new paragraph will be created in the details. It accepts either raw text or HTML. If empty, only the title will be rendered.
+
+Note the values of `title` and `lead` can be raw text or HTML.
+
+### sections
+
+The `sections` property expects an array of Section Objects. For each object, you will use Rule Objects to define items in the checklist’s section.
+
+#### Section Object
+
+A Section Object is made of 4 properties:
+
+```
+{
+  "name": "string",
+  "skippable": boolean,
+  "heading": "string",
+  "rules": [Rule Object]
+}
+```
+
+The `name` is the name of the section, that will be used for the labels’ input (`name` property), the section’s and heading’s `id`. **It MUST consequently be unique and abide by [`id`’s limitations and compatibility issues](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id).**
+
+The `skippable` property expects a boolean i.e. `true` or `false`. When set to `true`, a button to skip the section will be created – when clicking this button, all boxes are checked, and the user is sent to the next section. This may come in handy if your checklist has a set of rules that don’t apply to every project you manage.
+
+The `heading` property expects a string. It will be used for the `h2` of the section. The value can be raw text or HTML.
+
+The `rules` property expects an array of Rule Objects. This basically lists all the labels for the current section.
+
+#### Rule Object
+
+A Rule Object is made of 3 properties:
+
+```
+{
+  "value": "string",
+  "summary": "string",
+  "details": ["string", "another-string"]
+}
+```
+
+The `value` property expects a string. It will be used for the `value` of the `input`. **It MUST be unique.**
+
+The `summary` property expects a string. It will be used as the `label` of the input. When clicking this label, details will be displayed/hidden. The value can be raw text or HTML.
+
+The `details` property expects an array of strings. For each value in the array, a new paragraph will be created in the details. It accepts either raw text or HTML. If empty, “No details available :(” will be used as the default – as a means to encourage proper documentation.
+
+### footer
+
+The `footer` property expects an array of strings. For each value in the array, a new paragraph will be created in the footer. It accepts either raw text or HTML. If empty, only the version will be rendered.
+
+## Notes
+
+- Using Mustache is completely optional, you can use the existing markup in `index.html` if you are more comfortable with HTML – you should be fine as long as you follow the templating rules.
+- Mustache (and `data.json`) are not used at runtime, they are meant to generate a static HTML file locally/server-side.
+- The Web App is designed to run without JavaScript, albeit minimally (no interaction, no automatic save, etc.).
+- Everything is handled client-side (in the browser), no data is sent to any server and is therefore completely private – it’s not even shareable with co-workers.
+
+## FAQ
+
+### Why JSON, why!?
+
+It’s not about JSON but Mustache, a templating system that makes it way easier to create, maintain and update checklists. For the past 3 years, maintenance and updates have been a huge downside of BlitzOptim and BlitzDesign, as the update process was highly error-prone.
+
+With Mustache, data is now separated from markup. And it’s a lot easier to modify/generate the markup in one place instead of fifty.
+
+Finally it’s available on a large amount of different platforms, so should make it easier to use the data in a non-web context.
+
+### Can I keep using v1 of BlitzOptim and BlitzDesign?
+
+Yes, absolutely. 
+
+If you don’t want to migrate to Mustache and are more comfortable with the idea of manually maintaining the checklist, then you can use the `v.1.0.4` tag in your fork. 
+
+Note you’ll have to backport fixes and improvements though, as there won’t be any further development for this version.
---
+++ js/script.js
@@ -0,0 +1,351 @@
+r(function () {
+  // CUSTOM
+
+  var STORAGE_PREFIX = "blitzChecklist_";
+
+  var HELP_TEXT = `<div class='wrapper'>
+    <p>If you’re using a mouse or a finger:</p>
+    <ul>
+      <li>click the checkbox to check;</li>
+      <li>click the label to display details;</li>
+      <li>click the “Skip Section” button if the section doesn’t apply;</li>
+      <li>click reset to… reset the checklist;</li>
+    </ul>
+    <p>If you’re using a keyboard:</p>
+    <ul>
+      <li>press “tab” to navigate items;</li>
+      <li>press “enter” to check;</li>
+      <li>press “space” to display details;</li>
+      <li>press “esc” to reset the checklist;</li>
+    </ul>
+    <p>Don’t worry, your checklist is autosaved: you can close this page, your current checklist will be retrieved when reopened.</p>
+    <p>Finally, this is a Progressive Web App you can install on platforms and browsers which support it. It will even work offline if Service Workers are supported.</p>
+  </div>`;
+
+  // VARIABLES
+
+  var form = document.querySelector("form");
+  var details = document.querySelectorAll(".details");
+  var boxes = document.querySelectorAll("input[type='checkbox']");
+
+  var howTo = document.createElement("section");
+  var helper = document.createElement("button");
+  var help = document.createElement("div");
+
+  var skippable = document.querySelectorAll("[data-skippable='true']");
+
+  var controls = document.querySelector("#controls");
+  var barWrap = document.createElement("div");
+  var bar = document.createElement("div");
+
+  var count = boxes.length;
+  var checked = 0;
+  var progress = 0;
+
+  var isFirefox = false;
+
+  var mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+  var noMotion = false;
+
+  // FUNCTIONS
+
+  function getClosest(elem, tag) {
+    for (; elem && elem !== document && elem.nodeType === 1; elem = elem.parentNode) {
+      if (elem.tagName.toLowerCase() === tag) {
+        return elem;
+      }
+    }
+    return null;
+  };
+
+  function barHandler(widthPer) {
+    bar.style.width = widthPer + "%";
+    bar.dataset.width = widthPer + "%";
+  };
+
+  function updateProgress() {
+    checked = document.querySelectorAll("input[type='checkbox']:checked").length;
+    progress = parseInt(((checked / count) * 100), 10);
+    barHandler(progress);
+  };
+
+  function scrollTo(element, to, duration) {
+    if (!noMotion) {
+      if (duration <= 0) return;
+      var difference = to - element.scrollTop;
+      var perTick = difference / duration * 10;
+      setTimeout(function () {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop == to) return;
+        scrollTo(element, to, duration - 10);
+      }, 10);
+    }
+  };
+
+  function scrollTop() {
+    scrollTo(document.scrollingElement, form.offsetTop, 600);
+  };
+
+  function focusNoScroll(el) {
+    var x = window.scrollX;
+    var y = window.scrollY;
+    el.focus();
+    window.scrollTo(x, y);
+  };
+
+  function resetChecklist() {
+    localStorage.clear();
+    barHandler(0);
+
+    var enable = document.querySelectorAll("button[disabled]");
+    for (var i = 0; i < enable.length; i++) {
+      enable[i].disabled = false;
+    }
+
+    var uncheck = document.querySelectorAll("input[type='checkbox']:checked");
+    for (var i = 0; i < uncheck.length; i++) {
+      uncheck[i].checked = false;
+    }
+
+    focusNoScroll(document.querySelector("input[name]"));
+    scrollTop();
+  };
+
+  function toggleAria(el) {
+    if (el.getAttribute("aria-hidden") === "true") {
+      el.setAttribute("aria-hidden", "false");
+    } else {
+      el.setAttribute("aria-hidden", "true");
+    }
+  };
+
+  function toggleDetails(trigger) {
+    var detail = trigger.nextElementSibling;
+    trigger.classList.toggle("open");
+    detail.classList.toggle("hidden");
+    toggleAria(detail);
+  };
+
+  function toggleHelp() {
+    help.classList.toggle("hidden");
+    toggleAria(help);
+  };
+
+  function checkChildren(trigger) {
+    var sectionId = trigger.getAttribute("id");
+    var checkScope = sectionId.substring(0, sectionId.indexOf("-list"));
+    var toCheck = document.querySelectorAll("input[name='" + checkScope + "']");
+    for (var j = 0; j < toCheck.length; j++) {
+      var checkMe = toCheck[j];
+      checkMe.checked = true;
+      updateProgress();
+      localStorage.setItem(STORAGE_PREFIX + checkMe.getAttribute("value"), "true");
+      localStorage.setItem(STORAGE_PREFIX + "barWidth", progress);
+    }
+    trigger.setAttribute("disabled", "disabled");
+    localStorage.setItem(STORAGE_PREFIX + trigger.getAttribute("id") + "_button", "true");
+
+    // scroll to next section
+    var currentSection = getClosest(trigger, "section");
+    var nextSection = currentSection.nextElementSibling;
+    var nextButton = nextSection.querySelector(".checkAll");
+    var nextInput = nextSection.querySelector("input");
+    if (nextButton) {
+      focusNoScroll(nextButton);
+    } else {
+      focusNoScroll(nextInput);
+      if (isFirefox) {
+        nextInput.blur();
+        setTimeout(function () {
+          nextInput.focus();
+        }, 600);
+      }
+    }
+    scrollTo(document.scrollingElement, nextSection.offsetTop, 600);
+  };
+
+  function changeMotionHook() {
+    if (mediaQuery.matches) {
+      noMotion = true;
+    } else {
+      noMotion = false;
+    }
+  }
+
+  // INIT (run on doc Ready)
+
+  (function checkFirefox() {
+    if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+      isFirefox = true;
+    }
+  })();
+
+  (function initMotionHook() {
+    changeMotionHook();
+  })();
+
+  (function hideDetails() {
+    document.body.classList.add("js-enabled");
+    for (var i = 0; i < details.length; i++) {
+      var detail = details[i];
+      detail.classList.add("hidden");
+      detail.setAttribute("aria-hidden", "true");
+      detail.setAttribute("aria-live", "polite");
+    };
+  })();
+
+  (function initProgressBar() {
+    var retrievedProgress = localStorage.getItem(STORAGE_PREFIX + "barWidth");
+    barWrap.id = "progress";
+    bar.id = "progress-inner";
+    barWrap.appendChild(bar);
+    controls.insertBefore(barWrap, controls.firstChild);
+    if (retrievedProgress) {
+      barHandler(retrievedProgress);
+    } else {
+      setTimeout(function () {
+        updateProgress();
+        localStorage.setItem(STORAGE_PREFIX + "barWidth", progress);
+      }, 100);
+    };
+  })();
+
+  (function initCheckboxes() {
+    for (var i = 0; i < count; i++) {
+      var box = boxes[i];
+      if (box.hasAttribute("value")) {
+        var storageId = STORAGE_PREFIX + box.getAttribute("value");
+        var oldVal = localStorage.getItem(storageId);
+        box.checked = oldVal === "true" ? true : false;
+      }
+    };
+  })();
+
+  (function initCheckAllButtons() {
+    for (var i = 0; i < skippable.length; i++) {
+      var scope = skippable[i].id;
+      var value = scope.toLowerCase();
+      var button = document.createElement("button");
+      var disabled = localStorage.getItem(STORAGE_PREFIX + value + "_button");
+      button.type = "button";
+      button.classList.add("checkAll");
+      button.id = value;
+      button.innerHTML = "Skip section";
+      if (disabled) {
+        button.setAttribute("disabled", "disabled");
+      }
+      var section = document.querySelector("#" + value);
+      var wrapper = section.firstElementChild;
+      var firstLabel = wrapper.querySelector("label");
+      wrapper.insertBefore(button, firstLabel);
+    };
+  })();
+
+  (function initHelp() {
+    howTo.id = "how-to";
+
+    helper.type = "button";
+    helper.className = "helper";
+    helper.id = "helper";
+    helper.innerHTML = "Help";
+
+    howTo.appendChild(helper);
+
+    help.classList.add("help-content", "hidden");
+    help.id = "help";
+    help.setAttribute("aria-hidden", "true");
+    help.setAttribute("aria-live", "assertive");
+    help.innerHTML = HELP_TEXT;
+
+    howTo.appendChild(help);
+
+    helper.addEventListener("click", toggleHelp, false);
+
+    document.body.insertBefore(howTo, document.querySelector("main"));
+  })();
+
+  // Event Listeners 
+
+  mediaQuery.addListener(changeMotionHook);
+
+  form.addEventListener("click", function (e) {
+    var elt = e.target;
+    var isSummary = elt.classList.contains("summary");
+    var isSummaryNested = elt.matches(".summary > *");
+    var isDetailsPara = elt.classList.contains("details-para");
+    var isCheckAllButton = elt.classList.contains("checkAll");
+    var isReset = (elt.type === "reset");
+
+    if (isSummary) {
+      e.preventDefault();
+      toggleDetails(elt);
+    } else if (isSummaryNested) {
+      e.preventDefault();
+      toggleDetails(elt.parentElement);
+    } else if (isCheckAllButton) {
+      checkChildren(elt);
+    } else if (isReset) {
+      resetChecklist();
+    } else if (isDetailsPara) {
+      e.preventDefault();
+    } else {
+      return;
+    }
+  });
+
+  form.addEventListener("change", function (e) {
+    var elt = e.target;
+    var active = document.activeElement;
+    updateProgress();
+    if (elt.getAttribute("value") !== null) {
+      var storageId = STORAGE_PREFIX + elt.getAttribute("value");
+      var checkStatus = elt.checked;
+    } else {
+      var storageId = STORAGE_PREFIX + active.getAttribute("value");
+      var checkStatus = active.checked;
+    }
+    localStorage.setItem(STORAGE_PREFIX + "barWidth", progress);
+    localStorage.setItem(storageId, checkStatus);
+  });
+
+  if (isFirefox) {
+    document.addEventListener("keyup", keyboardHandler, false);
+  } else {
+    document.addEventListener("keydown", keyboardHandler, false);
+  }
+
+  function keyboardHandler(e) {
+    var active = document.activeElement;
+    var isCheckbox = (active.type === "checkbox");
+    var pressEnter = (e.key === "Enter" || e.keyCode === 13);
+    var pressEscape = (e.key === "Escape" || e.keyCode === 27);
+    var pressSpacebar = (e.key === "Spacebar" || e.keyCode === 32);
+
+    if (pressEscape) {
+      e.preventDefault();
+      resetChecklist();
+      if (isFirefox) {
+        active.blur();
+      };
+    } else if (isCheckbox && pressEnter) {
+      e.preventDefault();
+      var updateChange = new Event("change");
+      if (active.checked) {
+        active.checked = false;
+      } else {
+        active.checked = true;
+      };
+      form.dispatchEvent(updateChange);
+      if (!isFirefox) {
+        e.stopImmediatePropagation();
+      };
+    } else if (isCheckbox && pressSpacebar) {
+      e.preventDefault();
+      var pushActive = active.parentElement.querySelector(".summary");
+      toggleDetails(pushActive);
+    } else {
+      return;
+    }
+  };
+});
+function r(f) { /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f() }
